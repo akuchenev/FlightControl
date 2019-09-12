523c3abc760898820117172b80b1997b1e75daab
ihev <ihev@mail.ru>
2019-09-12 22:18:00 +0300

    Add bunny in crircle

--- src/app.js
+++ src/app.js
@@ -3,19 +3,42 @@ import * as PIXI from 'pixi.js'
 const app = new PIXI.Application({ transparent: true });
 document.body.appendChild(app.view);
 
-// create a new Sprite from an image path.
-const bunny = PIXI.Sprite.from('assets/bunny.png');
+var centrX = app.screen.width / 2;
+var centrY = app.screen.height / 2;
+
+
+var container = new PIXI.Container();
+container.x = centrX;
+container.y = centrY;
+
+var r = 100;
+var count = 20;
+for (let i = 0; i < count; i++) {
+    var alpha = i * 360 / count;
+    var x = r * Math.cos(alpha);
+    var y = r * Math.sin(alpha);
+    creatBunny( x, y);
+}
+
+
+app.stage.addChild(container);
+
+function creatBunny(x, y) {
+    // create a new Sprite from an image path.
+    const bunny = PIXI.Sprite.from('assets/bunny.png');
 
 // center the sprite's anchor point
-bunny.anchor.set(0.5);
+    bunny.anchor.set(0.5);
 
 // move the sprite to the center of the screen
-bunny.x = app.screen.width / 2;
-bunny.y = app.screen.height / 2;
+    bunny.x = x;
+    bunny.y = y;
 
-app.stage.addChild(bunny);
+    container.addChild(bunny);
+
+}
 
 app.ticker.add(() => {
     // just for fun, let's rotate mr rabbit a little
-    bunny.rotation += 0.1;
+    container.rotation += 0.01;
 });
