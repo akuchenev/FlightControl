f9582be53c6ce392ad63b8d443a2c2f0642af4c8
Eugene Khiznyakov <eugene.khiznyakov@gismart.com>
2019-09-12 18:59:09 +0300

    Add dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,30 +3,50 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 const outputDir = 'dist';
-module.exports = {
+module.exports = (env, argv) => {
+    const isDevMode = argv.mode === 'development';
+    const mode = isDevMode ? 'development' : 'production';
 
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, outputDir),
-        filename: 'bundle.js'
-    },
+    config = {
 
-    devServer: {
-        contentBase: path.join(__dirname, outputDir),
-        compress: false,
-        publicPath: '/',
-        https: false,
-        port: 3004,
-        overlay: {
-            warnings: true,
-            errors: true
-        }
-    },
+        entry: {
+ //           pixi: './node_modules/pixi.js',
+            app: './src/app.js',
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, outputDir),
+        },
+        mode: mode,
+        devtool: isDevMode ? 'source-map' : '',
+        // externals: [
+        //     {"pixijs": "PIXI"},
+        // ],
+        devServer: {
+            contentBase: path.join(__dirname, outputDir),
+            compress: false,
+            publicPath: '/',
+            https: false,
+            port: 3004,
+            overlay: {
+                warnings: true,
+                errors: true
+            }
+        },
 
-    plugins: [
-        new HtmlWebpackPlugin(),
-        new CopyPlugin([
-            { from: 'assets', to: 'assets' },
-        ]),]
+        plugins: [
+            new HtmlWebpackPlugin({
+                    hash: isDevMode,
+                    title: 'FC',
+                    minify: {
+                        collapseWhitespace: !isDevMode,
+                    },
+                }
+            ),
+            new CopyPlugin([
+                {from: 'assets', to: 'assets'},
+            ]),]
+    }
+    return config;
 };
 
